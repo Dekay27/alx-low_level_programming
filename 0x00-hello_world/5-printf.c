d7ee22010467bbae78747a201a896a3fae8ade28
Dekay27 <dannykumah27@gmail.com>
2021-06-16 16:39:54 -0700

    5-printf added

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-/** 
+/**
  * main - appending
- * Description: add a piece of sentence
+ * Description: with proper grammar, but the outcome is a piece of art
  * Return: 0
  */
 int main(void)
