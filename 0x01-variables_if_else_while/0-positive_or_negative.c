217e9822fe125d0586910ccc8ffebee4804a4c48
Dekay27 <dannykumah27@gmail.com>
2021-06-19 03:51:45 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,11 +16,11 @@ int main(void)
 
 	lastDigit = n % 10;
 	printf("Last digit of %i is %i and is ", n, lastDigit);
-	if (lastDigit > 5)
-		printf("greater than 5\n");
+	if (lastDigit > 0)
+		printf("is positive\n");
 	else if (lastDigit == 0)
-		printf("0\n");
+		printf("is zero\n");
 	else
-		printf("less than 6 and not 0\n");
+		printf("is negative\n");
 	return (0);
 }
