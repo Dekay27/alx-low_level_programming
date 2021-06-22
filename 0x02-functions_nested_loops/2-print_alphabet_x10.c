74e5aa2610a3c497dae3cc4b2724c09516abaa62
Dekay27 <dannykumah27@gmail.com>
2021-06-22 07:57:41 -0700

    0 edit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 
-/*
+/**
  * print_alphabet_x10 - prints alphabet 10 times
  * Return: NULL
  */
@@ -9,7 +9,7 @@ void print_alphabet_x10(void)
 	int i;
 	char c;
 
-	for (i=0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		{
