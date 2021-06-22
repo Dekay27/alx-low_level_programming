4a50c28d750e46e3ed415f2d6277a1df86499d83
Dekay27 <dannykumah27@gmail.com>
2021-06-22 08:08:53 -0700

    5 edited

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
  * print_sign - returns 1 if n is positive, -1 if negative and 0 if otherwise
- * @n: interger to be testes
+ * @n: integer to be testes
  * Return: 1, -1 or 0
  */
 int print_sign(int n)
@@ -16,7 +16,7 @@ int print_sign(int n)
 		_putchar('0');
 		return (0);
 	}
-	else if (n < 0)
+	else
 	{
 		_putchar('-');
 		return (-1);
